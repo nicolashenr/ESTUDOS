e31e1dc6909349c3cbb86ba9d3243d605524713c
Nicolas Henrique <nicolashenrique.barn@gmail.com>
2023-11-26 09:03:54 -0300

    Atualizado

---
+++ javascript/freecodecamp/beginners/intro/comment/script.js
@@ -0,0 +1,12 @@
+// assim é um comentário.
+
+// aqui de novo
+
+// E se nós quisermos
+
+/* nós podemos comentar assim,
+e aí podemos escrever várias linhas de comentários^^
+isso é bem legal para explicar. Ajudar quem vem aí*/
+
+var number = 2; // a partir daqui é tudo comentário.
+console.log(number);
---
+++ javascript/freecodecamp/beginners/intro/data-types-and-variables/script.js
@@ -0,0 +1,15 @@
+/* Data types
+undefined, null, boolean, string, symbol number, and object
+*/
+
+// Variables - Var, Let e Const
+
+var myName = "GarrafaAZul"
+
+myName = 159.8
+
+let folha = "verde"
+
+folha =  true;
+
+const pi =  3.14
