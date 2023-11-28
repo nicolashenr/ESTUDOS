a695ac1e1ba62ce0377134410034030d90246ee6
Nicolas Henrique <nicolashenrique.barn@gmail.com>
2023-11-28 17:00:18 -0300

    Atualizado

---
+++ Javascript/freecodecamp/beginners/intro/build-javascript-objects/script.js
@@ -0,0 +1,25 @@
+//Objetos são similares a arrays, mas ao invés de indices, usamos propriedades
+
+
+var doguinho = {
+    "name": "Spyke",
+    "legs": 8,
+    "tails": 1,
+    "friends": ["Everything!", "Everyone!"]
+};
+
+
+
+/*
+ACESSANDO PROPRIEDADES DO OBJETO COM NOTAÇÃO DE PONTO
+*/
+
+
+var nome = doguinho.name;
+var legs = doguinho.legs;
+console.log(nome);
+console.log(legs);
+
+var friends = doguinho.friends;
+console.log(friends);
+
